dcd20b205961ae2faeb686f7e59aeeb786de02d9
albertkun <albertk@gmx.com>
2020-02-19 14:29:30 -0800

    Added "logout" feature
    It's really a hack that redirects to the "homepage" that requires you to log back in, haha...

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,6 +3,7 @@
     <template slot="start">
       <b-navbar-item href="#/">Home</b-navbar-item>
       <b-navbar-item href="#/form">Create</b-navbar-item>
+      <b-navbar-item href="/">Logout</b-navbar-item>
     </template>
   </b-navbar>
 </template>
